8a5041e1b6bcf90b2eb76ba002ed2bc9b2cdf3ea
rauchrods <rauchrodrigues@gmail.com>
2024-06-23 23:42:57 +0530

    smiswrong

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -3,19 +3,20 @@ import "./sidebar.scss";
 import { MdLogout } from "react-icons/md";
 
 import { sideBarItems } from "../../assets/constants/sideBarItems";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const SideBar = () => {
+  const navigate = useNavigate();
   return (
     <div className="side-bar">
       <div className="header">
-        <h2>socializeME</h2>
+        <h2 onClick={() => navigate("/auth")}>socializeME</h2>
         <h2>sME</h2>
       </div>
 
       <div className="links">
         {sideBarItems.map((sideBarItem, index) => (
-          <Link to={sideBarItem.link}  key={index}>
+          <Link to={sideBarItem.link} key={index}>
             <div className="item">
               {sideBarItem.icon}
               <p>{sideBarItem.text}</p>
